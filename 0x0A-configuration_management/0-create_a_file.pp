0a2c310f7b703cd2108ce190a7a030a98d9901f3
Lulekoh <lulekoleoquintus12@gmail.com>
2023-11-24 14:54:33 -0800

    Configuration management updates

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file in /tmp with the following content
+
+file { '/tmp/school':
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
